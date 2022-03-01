67386b9d5e499dadd53444bbca749da30a297666
Dorian <dorianemenir@gmail.com>
2022-03-01 14:04:47 +0100

    :construction: removed unnecessary space

--- src/components/Watched.js
+++ src/components/Watched.js
@@ -10,7 +10,6 @@ export const Watched = () => {
       <div className="container">
         <div className="header">
           <h1 className="heading">Watched Movies</h1>
-
           <span className="count-pill">
             {watched.length} {watched.length === 1 ? "Movie" : "Movies"}
           </span>
